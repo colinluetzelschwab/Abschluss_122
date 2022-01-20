24973de1980ac53a559df7ba316f53cd97f673e9
colinluetzelschwab <colin.luetzelschwab@gmail.com>
2022-01-20 15:35:51 +0100

    hello

--- git-automation.sh
+++ git-automation.sh
@@ -4,6 +4,7 @@
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 GREEN='\033[0;32m'
+NC='\033[0m' #No Color
 
 # current Git branch
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
@@ -11,8 +12,8 @@ branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 # current project name
 projectName=$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')
 
-echo "Project: ${BLUE}$projectName"
-echo "Branch: ${PURPLE}$branch"
+echo "Project: ${BLUE}$projectName${NC}"
+echo "Branch: ${PURPLE}$branch${NC}"
 
 git add .
 
