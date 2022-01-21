56adf1c5a576aee05dceb3f50e72b5a5d2660b4c
colinluetzelschwab <colin.luetzelschwab@gmail.com>
2022-01-21 09:38:23 +0100

    lol

--- git-automation.sh
+++ git-automation.sh
@@ -12,7 +12,16 @@ branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 # current project name
 projectName=$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')
 
-echo "Project: ${BLUE}$projectName${NC}"
+echo "What dou you want to do?"
+echo "for pushin a project, press 1"
+echo "for pulling a project, press 2"
+echo "for changing to another branch, press 3"
+
+read number
+
+if [[ $number -eq 1 ]]
+then
+  echo "Project: ${BLUE}$projectName${NC}"
 echo "Branch: ${PURPLE}$branch${NC}"
 
 git add .
@@ -28,3 +37,13 @@ echo "$msg"
 git push
 
 echo "${GREEN}pushed"
+
+elif [[ $number -eq 2 ]]
+then
+  echo "The variable is equal to 2."
+elif [[ $number -eq 3 ]]
+then
+  echo "The variable is 3."
+else 
+    echo "select a valid number"
+fi
